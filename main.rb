bcf6585ed26f2effc78b9b27432ceafa2337e6a7
April Canete <aprilrose.canete93@gmail.com>
2022-01-12 11:24:11 +1100

    init commit

---
+++ main.rb
@@ -0,0 +1,31 @@
+     
+require 'sinatra'
+require 'sinatra/reloader' # turn off when deploying
+require 'pg'
+
+def db_query(sql, params = [])
+  conn = PG.connect(dbname: 'goodfoodhunting')
+  result = conn.exec_params(sql, params)
+
+  conn.close
+
+  result
+end
+
+def all_dishes()
+  db_query('select * from dishes;')
+end
+
+get '/' do
+  dishes = all_dishes()
+
+  erb :index, locals: {dishes: dishes}
+end
+
+get '/about' do
+  erb :about
+end
+
+
+
+
